f103f666637383678b8aaa75d85da0a1997ae73b
David <shamisdavid@gmail.com>
2024-05-20 08:25:10 -0700

    Crash malfunction Debug

--- backend/app.js
+++ backend/app.js
@@ -5,23 +5,20 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
 const app = express();
 
 app.use(morgan('dev'));
-
 app.use(cookieParser());
 app.use(express.json());
 
+
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
     app.use(cors());
   }
-
   // helmet helps set a variety of headers to better secure your app
   app.use(
     helmet.crossOriginResourcePolicy({
@@ -42,8 +39,6 @@ if (!isProduction) {
 
 
 const routes = require('./routes');
-
-// ...
 app.use(routes); // Connect all the routes
 
 // Catch unhandled requests and forward to error handler.
@@ -55,10 +50,7 @@ app.use((_req, _res, next) => {
   next(err);
 });
 
-// ...
 const { ValidationError } = require('sequelize');
-
-// ...
 
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
